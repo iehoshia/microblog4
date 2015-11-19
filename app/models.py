b1129ebc35da7630ae75a02d081ad7330ca41eb3
iehoshia <jepgez@gmail.com>
2015-11-19 11:06:28 -0600

    Actualización
    Actualización

--- app/models.py
+++ app/models.py
@@ -1,7 +1,7 @@
 from app import db
+from flask.ext.login import UserMixin, current_user, login_user, logout_user
 
-
-class User(db.Model):
+class UserDB(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     nickname = db.Column(db.String(64), index=True, unique=True)
     email = db.Column(db.String(120), index=True, unique=True)
@@ -19,3 +19,37 @@ class Post(db.Model):
 
     def __repr__(self):
         return '<Post %r>' % (self.body)
+
+class UserNotFoundError(Exception):
+    pass
+
+
+# http://flask-login.readthedocs.org/en/latest/_modules/flask/ext/login.html#UserMixin
+class User(UserMixin):
+    '''Simple User class'''
+    USERS = {
+        # username: password
+        'john': 'john',
+        'mary': 'mary'
+    }
+
+    def __init__(self, id):
+        if not id in self.USERS:
+            raise UserNotFoundError()
+        self.id = id
+        self.password = self.USERS[id]
+
+    @classmethod
+    def get(self_class, id):
+        '''Return user instance of id, return None if not exist'''
+        try:
+            return self_class(id)
+        except UserNotFoundError:
+            return None
+
+
+# Flask-Login use this to reload the user object from the user ID stored in the session
+@login_manager.user_loader
+def load_user(id):
+    return User.get(id)
+
--- app/views.py
+++ app/views.py
@@ -2,10 +2,8 @@ from flask import render_template, flash, redirect
 from app import app
 from .forms import LoginForm
 
-
-@app.route('/')
-@app.route('/index')
-def index():
+@app.route('/indexdb')
+def indexdb():
     user = {'nickname': 'Miguel'}
     posts = [
         {
@@ -17,20 +15,51 @@ def index():
             'body': 'The Avengers movie was so cool!'
         }
     ]
-    return render_template('index.html',
+    return render_template('indexdb.html',
                            title='Home',
                            user=user,
                            posts=posts)
 
 
-@app.route('/login', methods=['GET', 'POST'])
-def login():
+@app.route('/logindb', methods=['GET', 'POST'])
+def logindb():
     form = LoginForm()
     if form.validate_on_submit():
         flash('Login requested for OpenID="%s", remember_me=%s' %
               (form.openid.data, str(form.remember_me.data)))
-        return redirect('/index')
-    return render_template('login.html',
+        return redirect('/indexdb')
+    return render_template('logindb.html',
                            title='Sign In',
                            form=form,
                            providers=app.config['OPENID_PROVIDERS'])
+
+@app.route('/')
+def index():
+    
+    user = current_user.get_id() or 'Guess'
+    return render_template ('index.html', user = user)
+
+
+@app.route('/login')
+def login():
+    
+    return render_template('login.html' )
+
+
+@app.route('/login/check', methods=['post'])
+def login_check():
+    # validate username and password
+    user = User.get(request.form['username'])
+    if (user and user.password == request.form['password']):
+        login_user(user)
+    else:
+        flash('Username or password incorrect')
+
+    return redirect(url_for('index'))
+
+
+@app.route('/logout')
+def logout():
+    logout_user()
+    return redirect(url_for('index'))
+
